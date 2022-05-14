f2162439982616b1cdf708a5bc3a3edce4ae4a00
Neel Sortur <sortur.n@northeastern.edu>
2022-05-14 10:25:19 -0400

    config and scripts for baseline eval

---
+++ run.sh
@@ -0,0 +1,15 @@
+# ACTIVATE CONDA ENV BEFORE RUNNING
+jn="baseline"
+export TASK_MUJOCO=cup_catch
+
+start_dir=${jn}
+script=run_drqv2.sbatch
+
+jid[1]=$(./run_help.sh ${jn} ${start_dir} ${script} | tr -dc '0-9')
+echo ${jid[1]}
+
+# for j in {2..3}
+# do
+#   jid[${j}]=$(./run_help2.sh ${jn}_${j} ${jid[$((j-1))]} ${start_dir} ${script} | tr -dc '0-9')
+#   echo ${jid[$((j))]}
+# done
