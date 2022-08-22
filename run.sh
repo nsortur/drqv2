2cadcfd005a7b754adbf6799a509b947b784f0a5
Neel Sortur <sortur.n@northeastern.edu>
2022-08-21 21:27:34 -0400

    0821_2

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # ACTIVATE CONDA ENV BEFORE RUNNING
-jn="0820_2"
+jn="0821_2"
 echo ${jn}
 export TASK_MUJOCO=cartpole_swingup
 
--- train.py
+++ train.py
@@ -91,12 +91,16 @@ def enc_net(obs_shape, act, load_weights):
         enn.ReLU(enn.FieldType(act, n_out *
                                [act.regular_repr]), inplace=True),
         # 1x1
+        enn.R2Conv(enn.FieldType(act, n_out * [act.regular_repr]),
+                   enn.FieldType(act, 512 *
+                                 [act.regular_repr]),
+                   kernel_size=1, padding=0),
         
     )
     if load_weights:
         dict_init = torch.load(os.path.join(Path.cwd(), 'encWeights.pt'))
         net.load_state_dict(dict_init)
-    return net, 256
+    return net, 512
 
 
 def act_net(repr_dim, action_shape, act, load_weights):
@@ -133,8 +137,7 @@ def act_net(repr_dim, action_shape, act, load_weights):
 
     trunk = nn.Sequential(
         enn.R2Conv(enn.FieldType(act, repr_dim * [act.regular_repr]),
-                   enn.FieldType(act, 1024 *
-                                 [act.irrep(1)]),
+                   enn.FieldType(act, 1024 * [act.irrep(1)]),
                    kernel_size=1),
         enn.R2Conv(enn.FieldType(act, 1024 * [act.irrep(1)]),
                    enn.FieldType(act, feature_dim * [act.regular_repr]),
